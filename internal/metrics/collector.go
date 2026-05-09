ec7afbe561fde1f9ce10e49014911eee627115b2
absoluteqq <pashka21rus19@gmail.com>
2026-05-10 00:11:17 +0300

    started to code logic of collector...

--- internal/metrics/collector.go
+++ internal/metrics/collector.go
@@ -1,1 +1,43 @@
 package metrics
+
+import (
+	"context"
+	"time"
+)
+
+type Stats struct {
+	Total       int
+	Succeeded   int
+	Failed      int
+	Retried     int
+	AvgDuration time.Duration
+	P95Duration time.Duration
+}
+
+type Collector struct {
+	eventCh chan Event
+	done    chan struct{}
+}
+
+func NewCollector(bufSize int) *Collector
+
+func (c *Collector) Send(e Event) {
+	select {
+	// case <- c.done():
+	// 	return
+	case c.eventCh <- e:
+		return
+	default:
+		return
+	}
+}
+
+func (c *Collector) Wait() {} // TODO: implement the func
+
+func (c *Collector) Run(ctx context.Context) {} // TODO: implement the func
+
+func (c *Collector) Stats() {} // TODO: implement the func
+
+func (c *Collector) Stop() {
+	close(c.done) //wrong solution TODO: correct the func
+}
